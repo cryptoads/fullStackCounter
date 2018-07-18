e028ac943c0fffe6f2e3eca75e37f2cbb5b0eb26
Chris Michels <chrisjohnmichels@gmail.com>
2018-07-17 20:49:04 -0400

    first commit

---
+++ client/src/Counter.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+class Counter extends Component {
+    constructor(){
+        super()
+        this.state ={}
+    }
+
+    componentWillMount(){
+        axios.get('/counter')
+        .then(({ data }) =>{this.setState(data)})
+    }
+
+    addOne(){
+        console.log('this')
+        axios.get('/counter/add')
+        .then(({ data }) => {this.setState(data)})
+    }
+
+    subOne(){
+        axios.get('/counter/subtract')
+        .then(({ data }) => {this.setState(data)})
+    }
+
+    render(){
+        return(
+            <div>
+            <h1>{this.state.counter}</h1>
+            <button onClick={this.addOne.bind(this)}> + </button>
+            <button onClick={this.subOne.bind(this)}> - </button>
+            </div>
+            )
+    }
+}
+
+export default Counter; 
---
+++ routes/counter.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var router = express.Router();
+
+var counter = 0;
+
+router.get('/', function(req, res, next) {
+  res.json({counter: counter})
+});
+
+router.get('/add', function(req, res, next){
+    counter = counter + 1;
+    res.json({counter: counter});
+});
+
+router.get('/subtract', function(req, res, next){
+    counter = counter - 1;
+    res.json({counter:counter});
+});
+
+module.exports = router;
